50e1b604a6fae91f4540d356ff45587de9b9804c
felipemenezes937 <145990627+FelipeMenezes937@users.noreply.github.com>
2026-01-11 13:00:20 -0300

    error handling 2

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -25,7 +25,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     if(err instanceof UserAlredyExistsError){
       return reply.status(409).send()
     }
-    return reply.status(500).send() // TODO fix
+    throw err
   }
     return reply.status(201).send()
 }
